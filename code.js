0ba31ec33d184aa0a443c10a7a8881d39474df43
ishitac04 <ishitaa.chowdhury@gmail.com>
2025-07-28 18:18:14 +0100

    fixed random glitches and added background music/ start button

--- code.js
+++ code.js
@@ -17,6 +17,7 @@ let position = 0;
 let bullets = 10;
 let bulletsLeft = document.getElementById('bullets');
 let scoreText = document.getElementById('score')
+let gameStarted = false;
 
 function generateGrid() {
     let i=0;
@@ -31,108 +32,107 @@ function generateGrid() {
 }
 
 function generateApple(i) {
-    boxes[i+5].className = 'black';
-    boxes[i+205].className = 'lbrown';
-    boxes[i+204].className = 'black';
-    boxes[i+206].className = 'black';
-    boxes[i+406].className = 'black';
-    boxes[i+405].className = 'brown';
-    boxes[i+605].className = 'brown';
-    boxes[i+404].className = 'black';
-    boxes[i+407].className = 'black';
-    boxes[i+607].className = 'lgreen';
-    boxes[i+608].className = 'lgreen';
-    boxes[i+609].className = 'lgreen';
-    boxes[i+809].className = 'lgreen';
-    boxes[i+610].className = 'lgreen';
-    boxes[i+810].className = 'lgreen';
-    boxes[i+408].className = 'black';
-    boxes[i+807].className = 'green';
-    boxes[i+808].className = 'green';
-    boxes[i+1009].className = 'green';
-    boxes[i+1010].className = 'green';
-    boxes[i+1011].className = 'green';
-    boxes[i+811].className = 'green';
-    boxes[i+409].className = 'black';
-    boxes[i+410].className = 'black';
-    boxes[i+611].className = 'black';
-    boxes[i+812].className = 'black';
-    boxes[i+1012].className = 'black';
-    boxes[i+1212].className = 'black';
-    boxes[i+1211].className = 'black';
-    boxes[i+1210].className = 'black';
-    boxes[i+1209].className = 'black';
-    boxes[i+1008].className = 'black';
-    boxes[i+1007].className = 'black';
-    boxes[i+606].className = 'black';
-    boxes[i+604].className = 'black';
-    boxes[i+603].className = 'black';
-    boxes[i+803].className = 'red';
-    boxes[i+804].className = 'red';
-    boxes[i+1002].className = 'red';
-    boxes[i+1003].className = 'red';
-    boxes[i+1004].className = 'lred';
-    boxes[i+1005].className = 'lred';
-    boxes[i+1006].className = 'lred';
-    boxes[i+1203].className = 'lred';
-    boxes[i+1204].className = 'lred';
-    boxes[i+1205].className = 'lred';
-    boxes[i+1206].className = 'lred';
-    boxes[i+1405].className = 'lred';
-    boxes[i+1404].className = 'lred';
-    boxes[i+1403].className = 'lred';
-    boxes[i+1202].className = 'red';
-    boxes[i+1402].className = 'red';
-    boxes[i+1406].className = 'red';
-    boxes[i+1407].className = 'red';
-    boxes[i+1207].className = 'red';
-    boxes[i+1208].className = 'red';
-    boxes[i+1408].className = 'red';
-    boxes[i+1409].className = 'red';
-    boxes[i+1410].className = 'red';
-    boxes[i+1610].className = 'red';
+    boxes[i+5].classList.add("black");
+    boxes[i+205].classList.add("lbrown");
+    boxes[i+204].classList.add("black");
+    boxes[i+206].classList.add("black");
+    boxes[i+406].classList.add("black");
+    boxes[i+405].classList.add("brown");
+    boxes[i+605].classList.add("brown");
+    boxes[i+404].classList.add("black");
+    boxes[i+407].classList.add("black");
+    boxes[i+607].classList.add("lgreen");
+    boxes[i+608].classList.add("lgreen");
+    boxes[i+609].classList.add("lgreen");
+    boxes[i+809].classList.add("lgreen");
+    boxes[i+610].classList.add("lgreen");
+    boxes[i+810].classList.add("lgreen");
+    boxes[i+408].classList.add("black");
+    boxes[i+807].classList.add("green");
+    boxes[i+808].classList.add("green");
+    boxes[i+1009].classList.add("green");
+    boxes[i+1010].classList.add("green");
+    boxes[i+1011].classList.add("green");
+    boxes[i+811].classList.add("green");
+    boxes[i+409].classList.add("black");
+    boxes[i+410].classList.add("black");
+    boxes[i+611].classList.add("black");
+    boxes[i+812].classList.add("black");
+    boxes[i+1012].classList.add("black");
+    boxes[i+1212].classList.add("black");
+    boxes[i+1211].classList.add("black");
+    boxes[i+1210].classList.add("black");
+    boxes[i+1209].classList.add("black");
+    boxes[i+1008].classList.add("black");
+    boxes[i+1007].classList.add("black");
+    boxes[i+606].classList.add("black");
+    boxes[i+604].classList.add("black");
+    boxes[i+603].classList.add("black");
+    boxes[i+803].classList.add("red");
+    boxes[i+804].classList.add("red");
+    boxes[i+1002].classList.add("red");
+    boxes[i+1003].classList.add("red");
+    boxes[i+1004].classList.add("lred");
+    boxes[i+1005].classList.add("lred");
+    boxes[i+1006].classList.add("lred");
+    boxes[i+1203].classList.add("lred");
+    boxes[i+1204].classList.add("lred");
+    boxes[i+1205].classList.add("lred");
+    boxes[i+1206].classList.add("lred");
+    boxes[i+1405].classList.add("lred");
+    boxes[i+1404].classList.add("lred");
+    boxes[i+1403].classList.add("lred");
+    boxes[i+1202].classList.add("red");
+    boxes[i+1402].classList.add("red");
+    boxes[i+1406].classList.add("red");
+    boxes[i+1407].classList.add("red");
+    boxes[i+1207].classList.add("red");
+    boxes[i+1208].classList.add("red");
+    boxes[i+1408].classList.add("red");
+    boxes[i+1409].classList.add("red");
+    boxes[i+1410].classList.add("red");
+    boxes[i+1610].classList.add("red");
     for (let n=1; n<9; n++) {
-        boxes[i+1610-n].className = 'red';
+        boxes[i+1610-n].classList.add("red");
     }
     for (let n=0; n<9; n++) {
-        boxes[i+1810-n].className = 'red';
+        boxes[i+1810-n].classList.add("red");
     }
     for (let n=0; n<7; n++) {
-        boxes[i+2009-n].className = 'red';
+        boxes[i+2009-n].classList.add("red");
     }
-    boxes[i+2208].className = 'red';
-    boxes[i+2207].className = 'red';
-    boxes[i+2205].className = 'red';
-    boxes[i+2204].className = 'red';
-    boxes[i+802].className = 'black';
-    boxes[i+1001].className = 'black';
-    boxes[i+1201].className = 'black';
-    boxes[i+1401].className = 'black';
-    boxes[i+1601].className = 'black';
-    boxes[i+1801].className = 'black';
-    boxes[i+2002].className = 'black';
-    boxes[i+2203].className = 'black';
-    boxes[i+2404].className = 'black';
-    boxes[i+2405].className = 'black';
-    boxes[i+2206].className = 'black';
-    boxes[i+2407].className = 'black';
-    boxes[i+2408].className = 'black';
-    boxes[i+2209].className = 'black';
-    boxes[i+2010].className = 'black';
-    boxes[i+806].className = 'black';
-    boxes[i+805].className = 'black';
-    boxes[i+1411].className = 'black';
-    boxes[i+1611].className = 'black';
-    boxes[i+1811].className = 'black';
+    boxes[i+2208].classList.add("red");
+    boxes[i+2207].classList.add("red");
+    boxes[i+2205].classList.add("red");
+    boxes[i+2204].classList.add("red");
+    boxes[i+802].classList.add("black");
+    boxes[i+1001].classList.add("black");
+    boxes[i+1201].classList.add("black");
+    boxes[i+1401].classList.add("black");
+    boxes[i+1601].classList.add("black");
+    boxes[i+1801].classList.add("black");
+    boxes[i+2002].classList.add("black");
+    boxes[i+2203].classList.add("black");
+    boxes[i+2404].classList.add("black");
+    boxes[i+2405].classList.add("black");
+    boxes[i+2206].classList.add("black");
+    boxes[i+2407].classList.add("black");
+    boxes[i+2408].classList.add("black");
+    boxes[i+2209].classList.add("black");
+    boxes[i+2010].classList.add("black");
+    boxes[i+806].classList.add("black");
+    boxes[i+805].classList.add("black");
+    boxes[i+1411].classList.add("black");
+    boxes[i+1611].classList.add("black");
+    boxes[i+1811].classList.add("black");
 }
 
 function clearApple() {
     boxes.forEach(box => {
-        if (box.className=="red" || box.className=="black" || box.className=="green" || box.className=="lred" || box.className=="lbrown" || box.className=="brown" || box.className=="lgreen") {
-        box.textContent = " ";
-        box.className = '';
-        }
+        ["red","black","green","lred","lbrown","brown","lgreen"].forEach(c => {
+          box.classList.remove(c);
         });
+      });      
 }
 
 function ApplePosition() {
@@ -164,15 +164,15 @@ function moveApple() {
 function generateTarget(c) {
     const targetBoxes = [0, 200, 200, 1, 1, 2000, 1800, 1600, 1400, 1200, 6, 7, 8, 9, 10, 208, 408, 608, 607, 807, 806, 1006, 1005, 1205, 1204, 1404, 1403, 1603, 1602, 1601, 1599, 1598, 1597, 1397, 1396, 1196, 1195, 995, 994, 794, 793, 593, 592, 392, 192]
     for (let n = 0; n < targetBoxes.length; n++) {
-        boxes[c-targetBoxes[n]].className = "target";
-        boxes[c+targetBoxes[n]].className = "target";
+        boxes[c-targetBoxes[n]].classList.add("target");
+        boxes[c+targetBoxes[n]].classList.add("target");
       }
 }
 
 function clearTarget() {
     boxes.forEach(box => {
-        if (box.className === 'target') {
-            box.className = '';
+        if (box.classList.contains("target")) {
+            box.classList.remove("target");
         }
     });
 }
@@ -186,81 +186,73 @@ function shootBullet() {
     boxes[17045].className = "brown";
 }
 
-function setGame() {
-    position = ApplePosition();
-    generateApple(position);
-    appleInterval = setInterval(moveApple, 50);
-    shootBullet();
-
-
-document.addEventListener('keydown', (event) => { 
+function keyPress(event) {
     clearTarget();
     switch (event.key) {
         case 'ArrowUp':
-            currentDirection = 'up';
             centre = centre - 600;
-            if (lastDestroyed==currentApple) {
-                console.log("not creating it")
-            } else {
-            generateApple(position);
-            }
             break;
         case 'ArrowDown':
-            currentDirection = 'down';
             centre = centre + 600;
-            if (lastDestroyed==currentApple) {
-                console.log("not creating it")
-            } else {
-            generateApple(position);
-            }
             break;
         case 'ArrowLeft':
-            currentDirection = 'left';
             centre = centre - 3;
-            if (lastDestroyed==currentApple) {
-                console.log("not creating it")
-            } else {
-            generateApple(position);
-            }
             break;
         case 'ArrowRight':
-            currentDirection = 'right';
             centre = centre + 3;
-            if (lastDestroyed==currentApple) {
-                console.log("not creating it")
-            } else {
-            generateApple(position);
-            }
             break;
         case 'Enter':
-            currentDirection = 'enter';
             console.log("enter pressed");
             gunshot.play();
-            bullets=bullets-1;
+            bullets = bullets - 1;
             bulletsLeft.textContent = "Bullets left: " + bullets;
-            if (bullets==0) {
-                alert("Out of bullets! Your final score is "+ score)
+
+            if (bullets == 0) {
+                alert("Out of bullets! Your final score is " + score);
                 location.reload();
             }
 
-            if (boxes[centre].className == "red" || boxes[centre].className == "lred" || boxes[centre].className == "lgreen" || boxes[centre].className == "green" || boxes[centre].className == "black" || boxes[centre].className == "brown" || boxes[centre].className == "lbrown") {
+            if ((boxes[centre].classList.contains("red") || boxes[centre].classList.contains("lred"))) {
                 clearApple();
-                score = score + 1;
+                score=score+1;
                 scoreText.textContent = "Score: " + score;
-                console.log(score);
-                clearInterval(appleInterval);
-                centre=2500;
-                lastDestroyed=currentApple;
-                setGame();
-                console.log(currentApple, currentApple)
-                break;
+                centre = 2500;
+                lastDestroyed = currentApple;
+                setGame(); 
             }
-
             break;
     }
+
+    moveApple()
     generateTarget(centre);
-}); 
 }
 
-generateGrid();
-setGame()
+function setGame() {
+    position = ApplePosition();
+    generateApple(position);
+
+    if (appleInterval) {
+        clearInterval(appleInterval);
+    }
+    appleInterval = setInterval(moveApple, 50);
+
+    generateTarget(centre);
+}
+
+function startGame() {
+    generateGrid();
+    setGame();
+    document.addEventListener('keydown', keyPress);
+}
+
+function spaceStart(event) {
+    switch (event.key) {
+    case ' ':
+        document.removeEventListener('keydown', spaceStart);
+        gameStarted = true;
+        startGame();
+        backgroundmusic.play();
+    }
+}
+
+document.addEventListener('keydown', spaceStart)
