43fe6285f6ba7fbeab49e23770ce5636b78beca1
ishitac04 <ishitaa.chowdhury@gmail.com>
2025-07-30 10:22:26 +0100

    forgot to commit for ages i added levels and created a strawberry pixel by pixel and am fixing its movement

--- level2code.js
+++ level2code.js
@@ -19,7 +19,10 @@ let bulletsLeft = document.getElementById('bullets');
 let scoreText = document.getElementById('score')
 let gameStarted = false;
 const appleBoxes = [];
+const strawberryBoxes = [];
+let strawberryPosition = ApplePosition();
 const targetBoxes = [0, 200, 200, 1, 1, 2000, 1800, 1600, 1400, 1200, 6, 7, 8, 9, 10, 208, 408, 608, 607, 807, 806, 1006, 1005, 1205, 1204, 1404, 1403, 1603, 1602, 1601, 1599, 1598, 1597, 1397, 1396, 1196, 1195, 995, 994, 794, 793, 593, 592, 392, 192]
+currentStrawberry = 0;
 
 function generateGrid() {
     let i=0;
@@ -131,6 +134,47 @@ function generateApple(i) {
     createArray();
 }
 
+function createStrawberryArray() {
+    strawberryBoxes.length = 0;
+    for (let a=0; a<22000; a++) {
+        if (boxes[a].classList.contains("pink") || boxes[a].classList.contains("darkpink") || boxes[a].classList.contains("red") || boxes[a].classList.contains("darkred") || boxes[a].classList.contains("green")) {
+            strawberryBoxes.push(a);
+        }
+    }
+}
+
+function generateStrawberry(s) {
+    const blacksquares=[7,8,206, 209, 405, 404, 403, 410, 411,412,613,602, 801, 803,804,805,810,811,812,814,1001,1006,1009,1014,1200,1215,1400,1415,1600,1615,1800,1815,2000,2015,2201,2401,2602,2802,3003,3204,3405,3606,3607,3608,3609,3410,3211,3012,2813,2613,2414,2214]
+    const greensquares=[806,807,808,809,207,208,406,407,408,409,603,604,605,606,607,608,609,610,611,612]
+    const redsquares=[802,813,1002,1003,1004,1005,1007,1008,1010,1011,1012,1013,1201,1202,1203,1204,1205,1206,1207,1208,1209,1210,1211,1212,1213,1214,1401,1402,1403,1404,1405,1406,1407,1408,1409,1410,1411,1412,1413,1414,1601,1602,1603,1604,1605,1606,1607,1608,1609,1610,1611,1612,1613,1614,1801,1802,1803,1804,1805,1806,1807,1808,1809,1810,1811,1812,1813,1814,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2202,2203,2204,2205,2206,2207,2208,2209,2210,2211,2212,2213,2402,2403,2404,2405,2406,2407,2408,2409,2410,2411,2412,2413,2603,2604,2605,2606,2607,2608,2609,2610,2611,2612,2803,2804,2805,2806,2807,2808,2809,2810,2811,2812,3004,3005,3006,3007,3008,3009,3010,3011,3205,3206,3207,3208,3209,3210,3406,3407,3408,3409];
+    for (i=0;i<blacksquares.length;i++) {
+        a=s+blacksquares[i]
+        boxes[a].classList.add("sblack")
+    }
+    for (i=0;i<greensquares.length;i++) {
+        a=s+greensquares[i]
+        boxes[a].classList.add("sgreen")
+    }
+    for (i=0;i<redsquares.length;i++) {
+        a=s+redsquares[i]
+        if (i%4 == 0) {
+            boxes[a].classList.add("slred")
+        } else {
+            boxes[a].classList.add("sred")
+        }
+        
+    }
+}
+
+function clearStrawberry() {
+    strawberryBoxes.length = 0;
+    const strawberryColors = ["sred","slred","sblack","sgreen"];
+    for (let b=0; b < strawberryBoxes.length; b++) {
+        let pos = strawberryBoxes[b];
+        strawberryColors.forEach(color => boxes[pos].classList.remove(color));
+    }
+}
+
 function createArray() {
     appleBoxes.length = 0;
     for (let a=0; a<22000; a++) {
@@ -170,15 +214,22 @@ function moveApple() {
         }
     }
 
-
     generateApple(position);
+
+    clearStrawberry();
+    if (goingUp) {
+        strawberryPosition -= 196;
+    } else {
+        strawberryPosition += 204;
+    }
+    generateStrawberry(strawberryPosition);
 }
 
 function generateTarget(c) {
     for (let n = 0; n < targetBoxes.length; n++) {
         boxes[c-targetBoxes[n]].classList.add("target");
         boxes[c+targetBoxes[n]].classList.add("target");
-    }
+      }
 }
 
 function clearTarget() {
@@ -227,7 +278,7 @@ function keyPress(event) {
             }
             for (d=0; d<13; d++) {
                 const positions = [398, 399, 400, 401, 402, 198, 199, 200, 201, 202, 1, 2, 0];
-                if ((boxes[centre+positions[d]].classList.contains("red") || boxes[centre+positions[d]].classList.contains("lred") || boxes[centre+positions[d]].classList.contains("black") || boxes[centre+positions[d]].classList.contains("green") || boxes[centre+positions[d]].classList.contains("lgreen") || boxes[centre+positions[d]].classList.contains("brown") || boxes[centre+positions[d]].classList.contains("lbrown"))) {
+                if ((boxes[centre+positions[d]].classList.contains("darkpink") || boxes[centre+positions[d]].classList.contains("pink") || boxes[centre+positions[d]].classList.contains("darkred") || boxes[centre+positions[d]].classList.contains("red") || boxes[centre+positions[d]].classList.contains("lred") || boxes[centre+positions[d]].classList.contains("black") || boxes[centre+positions[d]].classList.contains("green") || boxes[centre+positions[d]].classList.contains("lgreen") || boxes[centre+positions[d]].classList.contains("brown") || boxes[centre+positions[d]].classList.contains("lbrown"))) {
                     clearApple();
                     score=score+1;
                     scoreText.textContent = "Score: " + score;
@@ -235,7 +286,7 @@ function keyPress(event) {
                     lastDestroyed = currentApple;
                     setGame(); 
                     break;
-                } else if ((boxes[centre+positions[d]].classList.contains("red") || boxes[centre-positions[d]].classList.contains("lred") || boxes[centre-positions[d]].classList.contains("black") || boxes[centre-positions[d]].classList.contains("green") || boxes[centre-positions[d]].classList.contains("lgreen") || boxes[centre-positions[d]].classList.contains("brown") || boxes[centre-positions[d]].classList.contains("lbrown"))) {
+                } else if ((boxes[centre+positions[d]].classList.contains("darkpink") || boxes[centre+positions[d]].classList.contains("pink") || boxes[centre+positions[d]].classList.contains("darkred") || boxes[centre+positions[d]].classList.contains("red") || boxes[centre-positions[d]].classList.contains("lred") || boxes[centre-positions[d]].classList.contains("black") || boxes[centre-positions[d]].classList.contains("green") || boxes[centre-positions[d]].classList.contains("lgreen") || boxes[centre-positions[d]].classList.contains("brown") || boxes[centre-positions[d]].classList.contains("lbrown"))) {
                 clearApple();
                 score=score+1;
                 scoreText.textContent = "Score: " + score;
@@ -258,8 +309,10 @@ function keyPress(event) {
 
 function setGame() {
     position = ApplePosition();
+    strawberryPosition = ApplePosition();
     generateApple(position);
-
+    generateStrawberry(strawberryPosition);
+    console.log(generateStrawberry)
     if (appleInterval) {
         clearInterval(appleInterval);
     }
@@ -269,6 +322,8 @@ function setGame() {
 }
 
 function startGame() {
+    bulletsLeft.textContent = "Bullets left: " + bullets;
+    scoreText.textContent = "Score: " + score;
     generateGrid();
     setGame();
     document.addEventListener('keydown', keyPress);
